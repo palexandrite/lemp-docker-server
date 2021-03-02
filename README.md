a24da7b243087ee710d52502e9642e2b32b0c1c8
malariuz <sb-101@yandex.ru>
2021-03-02 22:54:02 +0700

    It ready

---
+++ README.md
@@ -0,0 +1,7 @@
+# Lemp Docker Server
+
+Just clone & mount it.
+
+`git clone git@github.com:palexandrite/lemp-docker-server.git`
+
+`docker-compose up -d`
---
+++ domains/docker.test/www/index.php
@@ -0,0 +1,24 @@
+<?php
+
+echo 'This is docker test';
+
+// $mysqli = new mysqli('mysql', 'root', 'secret');
+
+// if (mysqli_connect_errno()) {
+//     die('the connection is down');
+// }
+
+// $mysqli->query('CREATE DATABASE testdb');
+
+// $mysqli->select_db('testdb');
+
+// $mysqli->query('CREATE TABLE test (name CHAR(20)');
+
+// $mysqli->query('INSERT INTO test VALUES ("roze")');
+
+// $result = $mysqli->query('SELECT name FROM test LIMIT 1');
+
+// echo '<pre>';
+// var_dump($result);
+
+// $mysqli->close();
